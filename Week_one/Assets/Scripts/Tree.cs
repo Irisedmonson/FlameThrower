114c6f0a5183b138ee8e4ab36067b9fb2cee76ac
Irisedmonson <ie009416@scc-mmlab-15.local>
2019-08-26 17:19:38 -0400

    Flame Thrower Project

---
+++ Week_one/Assets/Scripts/Tree.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+
+    public float CurrentHealth { get; set; }
+    public float MaxHealth { get; set; }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MaxHealth = 100f;
+        CurrentHealth = MaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void DealDamage(float damageValue)
+    {
+        CurrentHealth -= damageValue;
+        Debug.Log(CurrentHealth);
+        if (CurrentHealth <= 0)
+            Die();
+    }
+
+    void Die()
+    {
+        CurrentHealth = 0;
+        Debug.Log("you dead");
+        gameObject.SetActive(false);
+    }
+
+  private void OnParticleCollision(GameObject other)
+    {
+        DealDamage(1f);
+    }
+
+
+
+}
